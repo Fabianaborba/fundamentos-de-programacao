70d7ce4f883d5cd404735c2d13e0b17ec9c0e098
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2018-05-23 20:03:21 -0300

    Verifica se o número é primo ou não usando while

--- while/02-numero-primo.c
+++ while/02-numero-primo.c
@@ -2,27 +2,27 @@
 #include<stdlib.h>
 #include<locale.h>
 
-int main () {
+int main(){
   setlocale(LC_ALL, "Portuguese");
 
   int cont = 0,
       num1 = 0,
       div  = 0;
 
-  do {
+  do{
     printf("Digite um número: ");
     scanf("%d", &num1);
   }
 
-  while (num1 <= 0);
+  while(num1 <= 0);
     for (cont = 1; cont <= num1; cont++) {
       if (num1 % cont == 0) {
         div++;
       }
     }
-  if (div == 2) {
+  if(div == 2){
     printf("%d é primo!", num1);
-  } else {
+  }else{
     printf("%d não é primo!", num1);
   }
 
